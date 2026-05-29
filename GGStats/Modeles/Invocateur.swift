8684a797b743df4bef7516b55a255e737e274631
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2026-05-29 14:17:03 +0200

    écriture de la classe invocateur pour récup les infos d'un joueur

--- GGStats/Modeles/Invocateur.swift
+++ GGStats/Modeles/Invocateur.swift
@@ -5,3 +5,12 @@
 //  Created by Eve Lacroix on 29/05/2026.
 //
 
+import Foundation
+
+struct Invocateur: Codable {
+    let id: String
+    let puuid: String
+    let profileIconId: Int
+    let summonerLevel: Int
+}
+
