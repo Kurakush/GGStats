605202d37602745501138ab584ce5139f3a95f46
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2026-05-29 14:25:24 +0200

    ajout de la classe Classement + APIRiot

--- GGStats/Modeles/Classement.swift
+++ GGStats/Modeles/Classement.swift
@@ -5,3 +5,13 @@
 //  Created by Eve Lacroix on 29/05/2026.
 //
 
+import Foundation
+
+struct Classement: Codable {
+    let queueType: String
+    let tier: String
+    let rank: String
+    let leaguePoints: Int
+    let wins: Int
+    let losses: Int
+}
--- GGStats/Services/APIRiot.swift
+++ GGStats/Services/APIRiot.swift
@@ -5,3 +5,18 @@
 //  Created by Eve Lacroix on 29/05/2026.
 //
 
+import Foundation
+
+class APIRiot {
+    
+    private let cleAPI = "RGAPI-81547bf7-cfc5-4551-8ccf-912189a759e5"
+    
+    private let regionCompte = "europe"
+    private let regionLoL = "euw1"
+    
+    func rechercherJoueur(pseudo: String, tag: String) async throws -> (CompteRiot, Invocateur, Classement?) {
+        
+    }
+    
+    
+}
